7bd84a80c732c9d49749919501dfa8677f779441
Guillaume Turpin <guillaume.turpin45@gmail.com>
2025-04-24 19:34:01 +0200

    :recycle: Refactor routes into controller

---
+++ app/Http/Controllers/GithubRepositoryController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Contracts\GithubContract;
+
+class GithubRepositoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public static function index(string $owner, GithubContract $githubService)
+    {
+        return $githubService->getRepositories($owner);
+    }
+
+    // /**
+    //  * Show the form for creating a new resource.
+    //  */
+    // public function create()
+    // {
+    //     //
+    // }
+
+    // /**
+    //  * Store a newly created resource in storage.
+    //  */
+    // public function store(Request $request)
+    // {
+    //     //
+    // }
+
+    /**
+     * Display the specified resource.
+     */
+    public static function show(string $owner, string $repositoryName, GithubContract $githubService)
+    {
+        return $githubService->getRepository($owner, $repositoryName);
+    }
+
+    // /**
+    //  * Show the form for editing the specified resource.
+    //  */
+    // public function edit(string $id)
+    // {
+    //     //
+    // }
+
+    // /**
+    //  * Update the specified resource in storage.
+    //  */
+    // public function update(Request $request, string $id)
+    // {
+    //     //
+    // }
+
+    // /**
+    //  * Remove the specified resource from storage.
+    //  */
+    // public function destroy(string $id)
+    // {
+    //     //
+    // }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,16 @@
 <?php
 
 use App\Contracts\GithubContract;
+use App\Http\Controllers\GithubRepositoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-
-Route::get('/repositories/{owner}/{repositoryName}', function (string $owner, string $repositoryName, GithubContract $githubService) {
-    dd(
-        $owner,
-        $repositoryName,
-        $githubService->getRepository(
-            $owner,
-            $repositoryName,
-        ),
-    );
-})->name('repositories.show');
-
-Route::get('/repositories/{owner}', function (string $owner, GithubContract $githubService) {
-    dd(
-        $owner,
-        $githubService->getRepositories($owner),
-    );
-})->name('repositories.index');
+Route::prefix('repositories')
+    ->name('repositories.')
+    ->group(function () {
+        Route::get('/{owner}/{repositoryName}', GithubRepositoryController::show(...))->name('show');
+        Route::get('/{owner}', GithubRepositoryController::index(...))->name('index');
+    });
